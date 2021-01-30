0c26ab2c2bb7b1d0c8074303e2414c7942455305
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-01-30 14:21:13 +0300

    fixup! Создан компонент навигации + поправилена структура проекта

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Navigation } from "../index.js";
 
 function App() {
-  const navigationList = [
+  const [navigationList] = useState([
     { id: 1, title: "наши точки" },
     { id: 2, title: "френд-пасс" },
     { id: 3, isLogo: true, title: "coffee-point" },
     { id: 4, title: "журнал" },
     { id: 5, title: "наши точки" },
-  ];
+  ]);
   return (
     <div className="App">
       <Navigation navigationList={navigationList} />
