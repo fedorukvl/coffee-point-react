f2526ac7f5c60869df7e673914e6f3ffff3a5ff3
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-02-01 20:15:30 +0300

    Создан компонент слайдера

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 
-import { Navigation } from "../index.js";
+import { Navigation, PromoSlider } from "../index.js";
+
+export const Promotions = React.createContext(null);
 
 function App() {
   const [navigationList] = useState([
@@ -10,9 +12,43 @@ function App() {
     { id: 4, title: "журнал" },
     { id: 5, title: "наши точки" },
   ]);
+  const [promotionList] = useState([
+    {
+      id: 1,
+      name: "our-coffe-shops",
+      title: "Наши кофейни",
+      description:
+        "Мы постоянно совершенствуемся и стараемся быть ближе к тебе",
+    },
+    {
+      id: 2,
+      name: "magazine",
+      title: "Не только кофе",
+      description:
+        "С этого месяца у нас стартовал ежемесячный журнал о том, что близко тебе и нам по духу",
+    },
+    {
+      id: 3,
+      name: "friend-pass",
+      title: "Будь частью нашей культуры",
+      description:
+        "Не забудь оформить нашу карту, чтобы получать бонусы при заказах",
+    },
+    {
+      id: 4,
+      name: "events",
+      title: "Будь в курсе последних событий",
+      description: "Следи за нашими мероприятиями и принимай в них участие",
+    },
+  ]);
   return (
     <div className="App">
-      <Navigation navigationList={navigationList} />
+      <Fragment>
+        <Promotions.Provider value={promotionList}>
+          <Navigation navigationList={navigationList} />
+          <PromoSlider />
+        </Promotions.Provider>
+      </Fragment>
     </div>
   );
 }
